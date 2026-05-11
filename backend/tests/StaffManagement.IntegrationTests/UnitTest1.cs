25ca31985c8216b42b3056495930897be492244c
Sosakpanha Menh <131564407+Sosakpanha@users.noreply.github.com>
2026-05-11 21:46:27 +0700

    init empty project

---
+++ backend/tests/StaffManagement.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace StaffManagement.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ backend/tests/StaffManagement.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace StaffManagement.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
